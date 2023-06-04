594cfdb531e68b395a5c068b746028b3fd59de4e
Alexey Bobrov <q20100@mail.ru>
2023-06-04 12:30:43 +0500

    оброботка кнопки

--- src/bot/market-place-bot/market-place-bot.update.ts
+++ src/bot/market-place-bot/market-place-bot.update.ts
@@ -1,20 +1,23 @@
-import {MarketPlaceBotService} from './market-place-bot.service';
-import {InjectBot, Start, Update} from "nestjs-telegraf";
-import {Telegraf, Context} from 'telegraf'
-
+import { MarketPlaceBotService } from './market-place-bot.service';
+import { Telegraf, Context } from 'telegraf';
+import { actionButtons } from './button/button';
+import { Hears, InjectBot, Start, Update } from 'nestjs-telegraf';
 
 @Update()
 export class MarketPlaceBotUpdate {
-  constructor(@InjectBot() private readonly bot: Telegraf<Context>,  private readonly marketPlaceBotService: MarketPlaceBotService) {}
-
+  constructor(
+    @InjectBot() private readonly bot: Telegraf<Context>,
+    private readonly marketPlaceBotService: MarketPlaceBotService,
+  ) {}
 
   @Start()
   async startCommand(ctx: Context) {
-    await ctx.reply('Начало!')
+    await ctx.reply('Начало!');
+    await ctx.reply('Что будешь?', actionButtons());
   }
 
-
-
-
-
+  @Hears('Список дел')
+  async getAll() {
+    return 'авф-авф 🐕';
+  }
 }
